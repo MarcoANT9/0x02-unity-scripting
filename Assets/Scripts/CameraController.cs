f82c3ebb58d12d1b054b596aeff927bfeba08912
MarcoAnt9 <acalde27@hotmail.com>
2020-12-22 15:28:38 -0500

    Task 2: Added Camera Follow to the player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,44 +1,26 @@
 ﻿using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
     //================================================================================
     // Public Variables ==============================================================
     //================================================================================
-    private Rigidbody rb_player;
-    public float speed = 1000f;
+    public GameObject player;
+    public Vector3 offset;
 
     //================================================================================
     // Start is called before the first frame update =================================
     //================================================================================
     void Start()
     {
-            rb_player = GetComponent<Rigidbody>();
+        
     }
+
     //================================================================================
     // Update is called once per frame ===============================================
     //================================================================================
-    void FixedUpdate()
+    void Update()
     {
-        // Move Up
-        if (Input.GetKey("w") || Input.GetKey("up"))
-        {
-            rb_player.AddForce(0, 0, speed * Time.deltaTime);
-        }
-        // Move Down
-        if (Input.GetKey("s") || Input.GetKey("down"))
-        {
-            rb_player.AddForce(0, 0, -speed * Time.deltaTime);
-        }
-        // Move Left
-        if (Input.GetKey("a") || Input.GetKey("left"))
-        {
-            rb_player.AddForce(-speed * Time.deltaTime, 0, 0);
-        }
-        // Move Right
-        if (Input.GetKey("d") || Input.GetKey("right"))
-        {
-            rb_player.AddForce(speed * Time.deltaTime, 0, 0);
-        }
+        transform.position = player.transform.position + offset;
     }
 }
